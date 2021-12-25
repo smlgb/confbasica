562b58da37e2a188af804d4841150edd4a7f8721
smlgb <94322336+smlgb@users.noreply.github.com>
2021-12-25 19:33:30 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ mkdir ~/nanobak
 cp zshrc ~/.zshrc
 cp nanorc ~/.nanorc
 cp sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp sxhkdrc ~/.config/bspwm/sxhkd/sxhkdrc
 cp bspwmrc  ~/.config/bspwm/bspwmrc
 cp kitty.conf ~/.config/kitty/kitty.conf
 mv ~/.config/polybar ~/.config/polybar.bak 
