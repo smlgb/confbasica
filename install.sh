45aa2eca801834b1bbf412b1a3006c9dc694082f
smlgb <94322336+smlgb@users.noreply.github.com>
2021-12-25 21:51:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-yay -Sy kitty sxhkd polybar bspwm python ttf-feather ttf-iosevka-nerd libjsoncpp moc
+yay -Sy kitty sxhkd polybar bspwm python ttf-feather ttf-iosevka-nerd jsoncpp moc
 #sudo pacman -Sy kitty sxhkd polybar bspwm python ttf-feather ttf-iosevka-nerd
 mkdir ~/nanobak
 cp zshrc ~/.zshrc
