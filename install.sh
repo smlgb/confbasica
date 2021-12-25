63256f2cac22367ad3b6e6062b561a2466dd4464
smlgb <94322336+smlgb@users.noreply.github.com>
2021-12-25 19:37:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ cp nanorc ~/.nanorc
 cp sxhkdrc ~/.config/sxhkd/sxhkdrc
 cp sxhkdrc ~/.config/bspwm/sxhkd/sxhkdrc
 cp bspwmrc  ~/.config/bspwm/bspwmrc
+mkdir ~/.config/kitty
 cp kitty.conf ~/.config/kitty/kitty.conf
 mv ~/.config/polybar ~/.config/polybar.bak 
 cp -r polybar ~/.config/polybar
 chmod +x ~/.config/polybar/launch.sh
 chmod +x ~/.config/polybar/scripts/*
-pacman -Sy kitty sxhkd polybar bspwm
+sudo pacman -Sy kitty sxhkd polybar bspwm
 bspc wm -r
