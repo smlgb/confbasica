459d647244a372a087c8153eb19c061e75387f44
smlgb <94322336+smlgb@users.noreply.github.com>
2021-12-26 14:49:05 +0000

    Add files via upload

---
+++ polybar/scripts/applet_screenshot.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+dir="~/.config/polybar/scripts/rofi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+# Options
+pantalla=" Pantalla"
+area=" Area"
+ventana=" Ventana"
+suspend=" Sleep"
+logout=" Logout"
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $dir/confirm.rasi
+}
+
+# Message
+msg() {
+	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$area\n$pantalla\n$ventana"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Captura Pantalla" -dmenu -selected-row 0)"
+case $chosen in
+    $pantalla)
+		if [[ -f /usr/bin/scrot ]]; then
+			sleep 1; scrot 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+		else
+			msg
+		fi
+        ;;
+    $area)
+		if [[ -f /usr/bin/scrot ]]; then
+			scrot -s 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+		else
+			msg
+		fi
+        ;;
+    $ventana)
+		if [[ -f /usr/bin/scrot ]]; then
+			sleep 1; scrot -u 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
+		else
+			msg
+		fi
+        ;;
+esac
